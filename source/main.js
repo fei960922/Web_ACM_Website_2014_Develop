e15315a9cbb6c1ca6b9cfdd16f6f91bb8ee06c13
fei960922 <fei960922@sjtu.edu.cn>
2014-07-15 17:34:58 +0800

    V2.0
    New Index pages added!

--- source/main.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){
-  $(".p1").show("2000");
-  setTimeout('$(".p1").hide();$(".p2").fadeIn(1000);',1500);
-  setTimeout('$(".p2").hide();$(".p3").fadeIn(1000);',3000);
-})
