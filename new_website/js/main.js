6fa1158e0814df7a544da0132a67ce7b5829733b
fei960922 <fei960922@sjtu.edu.cn>
2014-07-18 11:18:57 +0800

    V3.0.0
    new website base added

---
+++ new_website/js/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    var winh=(window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight);
+    var h = winh - 100;
+    $(".main").css({height:h});
+    $(".academic_main").css({height:h});
+    h = (winh-100)/2;
+    $(".block_intro").css({height:h});
+    fontmaking();
+})
+
+/*	------------------------------------------
+	Inherit from "www.youziku.com"
+	------------------------------------------  */
+
+    function fontmaking() {
+        var resultStr = $(".Microsoft_YaHei").text();
+        var md5 = "";
+        resultStr = Trim(resultStr);
+        resultStr = SelectWord(resultStr);
+        md5 = $.md5("d6858636a9194575b81f6b3a5b3d1606"+"Microsoft_YaHei" + resultStr);
+        $.getJSON("http://www.youziku.com/webfont/CSSPOST?jsoncallback=?", { "id": md5, "guid": "d6858636a9194575b81f6b3a5b3d1606", "type": "5" }, function (json) {
+            if (json.result == 0) $.post("http://www.youziku.com/webfont/PostCorsCreateFont", { "name": "Microsoft_YaHei", "gid": "d6858636a9194575b81f6b3a5b3d1606", "type": "5", "text": resultStr });
+            else loadExtentFile("http://www.youziku.com/webfont/css?id=" + md5 + "&guid=" + "d6858636a9194575b81f6b3a5b3d1606" + "&type=5");
+        });
+    }
